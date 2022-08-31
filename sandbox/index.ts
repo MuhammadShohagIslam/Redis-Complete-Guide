c064cbb9d2f363c175d6588843296d9a46dc1745
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-31 14:36:47 +0300

    Parctice real code hash

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,16 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet("car", {
+        name: "Abc",
+        year: 1970
+    });
+    // HSET car name Abc year 1970
+
+    const car = await client.hGetAll("car");
+    // HGETALL car
+
+    console.log(car)
+};
 run();
