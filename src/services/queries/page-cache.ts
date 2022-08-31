8c07782a8e00cfc7049516f92ce1094d89645acc
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-31 10:19:06 +0300

    Done Adding Page Cache and generate key

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,25 @@
-export const getCachedPage = (route: string) => {};
+import { client } from '$services/redis';
 
-export const setCachedPage = (route: string, page: string) => {};
+import { pageCacheKey } from '$services/keys';
+
+// list of routes to cache
+const cachedRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
+
+export const getCachedPage = (route: string) => {
+	// is the route argument in the list of routes we want to cache
+	// if so, get the cached page from Redis,if not, return null
+	if (cachedRoutes.includes(route)) {
+		return client.get(pageCacheKey(route));
+	} else {
+		return null;
+	}
+};
+
+export const setCachedPage = (route: string, page: string) => {
+	// if the route argument in the list of routes we want to cache, if so, store the page in Redis
+	if (cachedRoutes.includes(route)) {
+		return client.set(pageCacheKey(route), page, {
+			EX: 4
+		});
+	}
+};
